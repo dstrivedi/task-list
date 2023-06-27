1ee3a164e3a6f92aa3a854b657f14a6413cde427
dstrivedi <dstrivedi@users.noreply.github.com>
2023-06-27 12:45:53 -0400

    1 file modified, 1 added

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import * as React from 'react';
 import './style.css';
 import Container from '@mui/material/Container';
+import TaskList from '../components/TaskList';
 
 export default function App() {
   return (
     <Container maxWidth="md">
-      <h1>Task list</h1>
+      <h1>Tasklist</h1>
     </Container>
   );
 }
